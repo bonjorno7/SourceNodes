91d99338e723a7238f4234ae879551ac0cb5da72
bonjorno7 <jorijndegraaf@gmail.com>
2021-06-22 04:29:52 +0200

    Ban reroute nodes, mark invalid links
    Reroutes give me too much of a headache, due to various reasons, so I'll just relink and remove them as soon as they're added.
    The invalid link check is very simple, just compares types, and I think that's fine.
    It gets called from a timer, because I think something gets called after update which resets the link flags, so I have to set them after whatever that is.
    Alternatively I could just remove the links as soon as they're made, but I think this is nicer.

--- node/tree.py
+++ node/tree.py
@@ -1,11 +1,39 @@
 import bpy
-from bpy.types import NodeTree
+from bpy.types import NodeReroute, NodeTree
 
 
 class SourceNodeTree(NodeTree):
     '''Node based export for Source models'''
     bl_label = 'Source Nodes'
     bl_icon = 'FILE_3D'
+
+    def update(self):
+        '''Called when node graph is changed'''
+        self.remove_reroute_nodes()
+        bpy.app.timers.register(self.mark_invalid_links)
+
+    def remove_reroute_nodes(self):
+        '''Remove reroute nodes and attempt to replace their links'''
+        for node in self.nodes:
+            if isinstance(node, NodeReroute):
+                reroute_input = node.inputs[0]
+                reroute_output = node.outputs[0]
+
+                if reroute_input.is_linked:
+                    input_link = reroute_input.links[0]
+                    output_socket = input_link.from_socket
+
+                    for output_link in reroute_output.links:
+                        input_socket = output_link.to_socket
+                        self.links.new(input_socket, output_socket)
+
+                self.nodes.remove(node)
+
+    def mark_invalid_links(self):
+        '''Mark invalid links, must be called from a timer'''
+        for link in self.links:
+            if type(link.from_socket) != type(link.to_socket):
+                link.is_valid = False
 
 
 classes = (SourceNodeTree,)
