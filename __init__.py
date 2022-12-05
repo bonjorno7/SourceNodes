b0605704e578fbb70cd5aa4326385900a05ac048
bonjorno7 <jorijndegraaf@gmail.com>
2022-12-05 17:30:07 +0100

    Raise minimum Blender version
    3.0.0 is probably high enough.
    I do all my testing in 3.3.0 at the moment.

--- __init__.py
+++ __init__.py
@@ -2,7 +2,7 @@ bl_info = {
     'name': 'Source Nodes',
     'author': 'bonjorno7',
     'description': 'Blender node based export for Source models',
-    'blender': (2, 80, 0),
+    'blender': (3, 0, 0),
     'version': (0, 0, 1),
     'location': 'Editors > Source Nodes',
     'category': 'Import-Export',
